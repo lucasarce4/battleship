f56b6ecb09d3b407200cbd977a27797aacd3848c
lucasarce4 <lucasearce4@gmail.com>
2022-10-20 04:42:22 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import gameboardFactory from './gameboardFactory';
-import shipFactory from './shipFactory';
-import { player, aiPlayer } from './players';
-import { generateBoards, placeShips } from './game';
-import drag from './dragAndDrop';
+import gameboardFactory from "./gameboardFactory";
+import shipFactory from "./shipFactory";
+import { player, aiPlayer } from "./players";
+import { generateBoards, placeShips } from "./game";
+import drag from "./dragAndDrop";
